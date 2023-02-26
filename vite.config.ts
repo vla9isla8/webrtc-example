a37543ee8af103fdf16e73279ada0f28cca07b84
Vladislav Kalasnikov <vladka.0804@gmail.com>
2023-02-26 08:21:30 +0800

    chore: asset's links

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: process.env.BASE_URL,
     plugins: [
         react()
     ],
-})
+});
